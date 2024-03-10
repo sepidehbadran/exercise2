9d01bdbe3ef741253fb624c2d63cf1dfbe035373
Sepideh <sepideh.badran05@gmail.com>
2024-03-10 12:16:05 +0330

    calculate y=31*x - 17*x +5

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Enter the value of x:");
+int x = Convert.ToInt32(Console.ReadLine());
+
+int y = ((x << 5) - x) - ((x << 4) + (x << 0)) + 5;
+
+Console.WriteLine($"Result: y = {y}");
+
